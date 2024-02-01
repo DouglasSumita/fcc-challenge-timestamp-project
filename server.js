09eb52b9633e521a572fb092f935b12288e9a253
Douglas Sumita <douglas.sumita@gmail.com>
2024-02-01 14:13:45 -0300

    I removed index.js because server.js has the same goal, updated package.json to start server.js, and added cors in server.js

--- server.js
+++ server.js
@@ -2,24 +2,12 @@ const express = require('express');
 const myApp = require('./myApp');
 const app = express();
 
-if (!process.env.DISABLE_XORIGIN) {
-  app.use((req, res, next) => {
-    const allowedOrigins = ['https://narrow-plane.gomix.me', 'https://www.freecodecamp.com'];
-    const origin = req.headers.origin || '*';
-    if(!process.env.XORIG_RESTRICT || allowedOrigins.indexOf(origin) > -1){
-         console.log(origin);
-         res.setHeader('Access-Control-Allow-Origin', origin);
-         res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-    }
-    next();
-  });
-}
+// enable CORS (https://en.wikipedia.org/wiki/Cross-origin_resource_sharing)
+// so that your API is remotely testable by FCC 
+var cors = require('cors');
+app.use(cors({optionsSuccessStatus: 200}));  // some legacy browsers choke on 204
 
 app.use('/', myApp);
-
-
-
-
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
